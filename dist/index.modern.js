eabc40d961e20ae0aeec40d60f6f4f14d4170605
kristinegusta <kristine.guta@maastrichtuniversity.nl>
2023-08-29 20:36:17 +0200

    Microbundle config

---
+++ dist/index.modern.js
@@ -0,0 +1,20 @@
+import"react";import t from"styled-components";let e,n,i,l=t=>t;const r=t=>h(Fragment,null,h(m,{textAlign:t.textAlign},t.subtitle),h(x,{textAlign:t.textAlign},t.title),h(a,null,t.content,t.link?h("a",{href:t.link},t.linkText):null)),m=t.h4(e||(e=l`
+  color: #f09125;
+  text-transform: uppercase;
+  text-align: ${0};
+  margin: 0.5rem 0;
+`),t=>"left"===t.textAlign?"left":"center"),x=t.h2(n||(n=l`
+  font-size: 28px;
+  text-align: ${0};
+  max-width: ${0};
+  margin: ${0};
+  @media ${0} {
+    margin: ${0};
+  }
+`),t=>"left"===t.textAlign?"left":"center",t=>"left"===t.textAlign?"unset":"650px",t=>"left"===t.textAlign?"0 0 1.5rem 0":"0 auto 1.5rem auto","(min-width: 1024px)",t=>"left"===t.textAlign?"0 0 1.5rem 0":"0 auto 3rem auto"),a=t.div(i||(i=l`
+  width: 100%;
+  @media ${0} {
+    max-width: 65ch;
+  }
+`),"(min-width: 768px)");export{r as Title};
+//# sourceMappingURL=index.modern.js.map
